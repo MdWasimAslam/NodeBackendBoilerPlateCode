19d3a9238fc10dba54ee048685b55f44e2bf289a
WasimAslam-entiovi <wasimaslam1998@gmail.com>
2026-02-19 01:38:12 +0530

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("OK - Node app is running"));
+app.get("/", (req, res) => res.send({status: "ok", message: "node server is running"}));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => console.log("Listening on", PORT));
